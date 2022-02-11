b53ea2230f502ffcb2951623d3332c74d14c12b7
MandoV0 <burakgulduz@gmail.com>
2022-02-11 09:40:58 +0100

    MVC

--- app/src/main/java/com/example/hanausightseeing/Controller.java
+++ app/src/main/java/com/example/hanausightseeing/Controller.java
@@ -1,4 +1,12 @@
 package com.example.hanausightseeing;
 
 public class Controller {
+
+    View mainView;
+    Model model;
+
+    public Controller() {
+        mainView = new View();
+        model = new Model();
+    }
 }
---
+++ app/src/main/java/com/example/hanausightseeing/Sehenswürdigkeit.java
@@ -0,0 +1,11 @@
+package com.example.hanausightseeing;
+
+public class Sehenswürdigkeit {
+    public String name;
+    public String beschreibung;
+
+    public Sehenswürdigkeit(String name, String beschreibung) {
+        this.beschreibung = beschreibung;
+        this.name = name;
+    }
+}
