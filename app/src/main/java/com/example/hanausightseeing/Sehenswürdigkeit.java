d0446b8c08e80f66800c0b04af2821277f3269b7
MandoV0 <burakgulduz@gmail.com>
2022-02-11 15:59:45 +0100

    Sight View

--- app/src/main/java/com/example/hanausightseeing/Sehenswürdigkeit.java
+++ app/src/main/java/com/example/hanausightseeing/Sehenswürdigkeit.java
@@ -1,8 +1,10 @@
 package com.example.hanausightseeing;
 
 public class Sehenswürdigkeit {
+
     public String name;
     public String beschreibung;
+    public String[] bilder;
 
     public Sehenswürdigkeit(String name, String beschreibung) {
         this.beschreibung = beschreibung;
