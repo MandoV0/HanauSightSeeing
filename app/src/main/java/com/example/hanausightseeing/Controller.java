55fc4cc9fbc3518af3a6f1c35f0c14f2d41f28c9
MandoV0 <burakgulduz@gmail.com>
2022-02-12 14:55:07 +0100

    MVC teilweise implementiert

--- app/src/main/java/com/example/hanausightseeing/Controller.java
+++ app/src/main/java/com/example/hanausightseeing/Controller.java
@@ -12,6 +12,6 @@ public class Controller {
     }
 
     void start() {
-        mainView.updateSehenswürdigkeiten(model.getSehenswürdigkeiten());
+        mainView.updateSehenswuerdigkeiten(model.getSehenswürdigkeiten());
     }
 }
--- app/src/main/java/com/example/hanausightseeing/MainActivity.java
+++ app/src/main/java/com/example/hanausightseeing/MainActivity.java
@@ -9,7 +9,6 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        //Controller c = new Controller(this);
         super.onCreate(savedInstanceState);
         // setContentView(R.layout.activity_main);
         setContentView(R.layout.sehenswuerdigkeit_design);
@@ -17,8 +16,8 @@ public class MainActivity extends AppCompatActivity {
         Controller c = new Controller(this);
     }
 
-    public void updateSehenswürdigkeiten(String[] sehenswürdigkeiten) {
+    public void updateSehenswuerdigkeiten(String[] sehenswuerdigkeiten) {
         TextView test = findViewById(R.id.sehenswürdigkeiten_name);
-        test.setText(sehenswürdigkeiten[1]);
+        test.setText(sehenswuerdigkeiten[1]);
     }
 }
