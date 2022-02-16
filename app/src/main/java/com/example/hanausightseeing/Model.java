de96590610fa73d92874e3df4704baa9e8dd09d8
MandoV0 <burakgulduz@gmail.com>
2022-02-16 11:38:44 +0100

    Design

--- app/src/main/java/com/example/hanausightseeing/Model.java
+++ app/src/main/java/com/example/hanausightseeing/Model.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Model {
+
     private ArrayList<Sehenswürdigkeit> sehenswürdigkeitList = new ArrayList<Sehenswürdigkeit>(Arrays.asList( new Sehenswürdigkeit("Goldschmiede", "Die Goldschmiede wurde 1990 gebaut", R.drawable.goldschmiede),
             new Sehenswürdigkeit("Schloss", "Das Schloss der Märchen", R.drawable.schloss),
             new Sehenswürdigkeit("Brüder-Grimm-Nationaldenkmal", "Das Brüder-Grimm-Nationaldenkmal ist eine Doppelstatue für die in Hanau geborenen Brüder Grimm. Sie wurde 1896 vor dem Neustädter Rathaus in Hanau enthüllt. ", R.drawable.grimm),
